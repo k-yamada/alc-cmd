15373f5cffefd893e1daee9be39d8bcf3b22fbc2
Kazuhiro Yamada <kyamada@sonix.asia>
2012-12-12 21:41:50 +0900

    Clean up cli.rb

--- lib/alc-cmd/cli.rb
+++ lib/alc-cmd/cli.rb
@@ -8,7 +8,17 @@ module Alc; module Cmd
 
     desc "s [WORD1 WORD2]", "serach words"
     def s(*words)
-      Eijiro.new.search(words)
+      result = Eijiro.new.search(words)
+      show_console(result)
+    end
+
+    private
+
+    def show_console(text)
+      tmp_file_name = "/tmp/alc.temp" + Time.now.to_f.ceil.to_s
+      File.open(tmp_file_name, "w") do |file| file.write(text) end
+      system("less -R #{tmp_file_name}")
+      system("rm -rf #{tmp_file_name}")
     end
 
   end
